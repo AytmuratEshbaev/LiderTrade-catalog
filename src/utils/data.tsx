5b46c006a36fed9375b8ef8cdb19bda92238cae3
Aytmurat Eshbaev <a.eshbaev1@gmail.com>
2023-04-30 23:31:51 +0500

    Save

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -8,47 +8,82 @@ const catalog: ICatalog = [
         sectionImg: "./images/z1.jpg",
         products: [
             {
-                productName: "asdasd",
+                productName: "uslada1",
                 productPrice: 12000,
-                productDescription: "asdaaaaaaaaaaaaaaaaaaaawqereewqrwerwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwewrwrwerewrrrrrrrrrrrrrrr",
-                productImg: "./images/z1.jpg",
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada1.jpg",
                 productId: 0
             },
             {
-                productName: "asdasd",
+                productName: "uslada2",
                 productPrice: 12000,
-                productDescription: "asdaaaaaaaaaaaaaaaaaaaawqereewqrwerwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwewrwrwerewrrrrrrrrrrrrrrr",
-                productImg: "./images/z1.jpg",
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada2.jpg",
                 productId: 1
             },
             {
-                productName: "asdasd",
+                productName: "uslada2",
                 productPrice: 12000,
-                productDescription: "asdaaaaaaaaaaaaaaaaaaaawqereewqrwerwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwewrwrwerewrrrrrrrrrrrrrrr",
-                productImg: "./images/z1.jpg",
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada2.jpg",
                 productId: 2
             },
             {
-                productName: "asdasd",
+                productName: "uslada3",
                 productPrice: 12000,
-                productDescription: "asdaaaaaaaaaaaaaaaaaaaawqereewqrwerwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwewrwrwerewrrrrrrrrrrrrrrr",
-                productImg: "./images/z1.jpg",
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada3.jpg",
                 productId: 3
             },
             {
-                productName: "asdasd",
+                productName: "uslada4",
                 productPrice: 12000,
-                productDescription: "asdaaaaaaaaaaaaaaaaaaaawqereewqrwerwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwewrwrwerewrrrrrrrrrrrrrrr",
-                productImg: "./images/z1.jpg",
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada4.jpg",
                 productId: 4
             },
             {
-                productName: "asdasd",
+                productName: "uslada5",
                 productPrice: 12000,
-                productDescription: "asdaaaaaaaaaaaaaaaaaaaawqereewqrwerwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwewrwrwerewrrrrrrrrrrrrrrr",
-                productImg: "./images/z1.jpg",
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada5.jpg",
                 productId: 5
             },
+            {
+                productName: "uslada6",
+                productPrice: 12000,
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada6.jpg",
+                productId: 6
+            },
+            {
+                productName: "uslada7",
+                productPrice: 12000,
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada7.jpg",
+                productId: 7
+            },
+            {
+                productName: "uslada8",
+                productPrice: 12000,
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada8.jpg",
+                productId: 8
+            },
+            {
+                productName: "uslada9",
+                productPrice: 12000,
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada9.jpg",
+                productId: 9
+            },
+            {
+                productName: "uslada10",
+                productPrice: 12000,
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/uslada10.jpg",
+                productId: 10
+            }
         ]
     },
     {
@@ -67,25 +102,64 @@ const catalog: ICatalog = [
         sectionId: 2,
         sectionName: 'Guzel сладости',
         sectionImg: "./images/z1.jpg",
-        products: [{
-            productName: "asdasd",
-            productPrice: 12,
-            productDescription: "",
-            productImg: "./images/z1.jpg",
-            productId: 0
-        }]
+        products: [
+            {
+                productName: "guzel1",
+                productPrice: 12,
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/guzel1.jpg",
+                productId: 0
+            },
+            {
+                productName: "guzel2",
+                productPrice: 12,
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/guzel2.jpg",
+                productId: 1
+            },
+            {
+                productName: "guzel3",
+                productPrice: 12,
+                productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+                productImg: "./images/guzel3.jpg",
+                productId: 2
+            },
+        ]
     },
     {
         sectionId: 3,
         sectionName: 'ULKER turk',
         sectionImg: "./images/z1.jpg",
-        products: [{
-            productName: "asdasd",
+        products: [
+            {
+            productName: "ulker turk1",
             productPrice: 12,
-            productDescription: "",
-            productImg: "./images/z1.jpg",
+            productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+            productImg: "./images/turk1.jpg",
             productId: 0
-        }]
+        },
+        {
+            productName: "ulker turk2",
+            productPrice: 12,
+            productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+            productImg: "./images/turk2.jpg",
+            productId: 1
+        },
+        {
+            productName: "ulker turk3",
+            productPrice: 12,
+            productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+            productImg: "./images/turk3.jpg",
+            productId: 2
+        },
+        {
+            productName: "ulker turk4",
+            productPrice: 12,
+            productDescription: "Zor hám sıpatlı tovar. Jetkizip beriw biypul.",
+            productImg: "./images/turk4.jpg",
+            productId: 3
+        },
+    ]
     },
     {
         sectionId: 4,
